1a945a804c8727bddd87b27a94831f60a72edbd2
wdix1850 <wdix1850@corp.sysco.com>
2021-09-24 10:37:21 -0500

    Initial Source, Test and Build files
    This is the initial implementation, test and build

---
+++ src/main/java/net/littlethunder/util/repeat/Repeat.java
@@ -0,0 +1,119 @@
+package net.littlethunder.util.repeat;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.IntConsumer;
+import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+/**
+ * A set of Utilities which repeat or sequentially execute things
+ */
+public final class Repeat {
+
+    /**
+     * Private constructor to prevent instantiation
+     */
+    private Repeat() {
+    }
+
+    /**
+     * Obtain a list of specified legth which contains references to a supplied object
+     *
+     * @param t   the Object to fill the list
+     * @param n   the length of the returned list
+     * @param <T> the type of the Object repeated
+     * @return a list of length n filled with references to t
+     */
+    public static <T> List<T> listOf(T t, int n) {
+        return (n > -1) ?
+            new ArrayList<>(Collections.nCopies(n, t)) : Collections.emptyList();
+    }
+
+    /**
+     * Create an infinite Stream of references to a supplied Object
+     *
+     * @param t   the Object to stream
+     * @param <T> the type of the Object to Stream
+     * @return a Stream which continually provides references to t
+     */
+    public static <T> Stream<T> streamOf(T t) {
+        return Stream.generate(() -> t);
+    }
+
+    /**
+     * Call an IntConsumer with a range of Integers, starting at 0.
+     *
+     * @param n        upper bound, exclusive. IN the case, the number of invocations as well
+     * @param consumer the IntConsumer invoked with each value in the range
+     */
+    public static void invokeRange(int n, IntConsumer consumer) {
+        //TODO Add error handling
+        invokeRange(0, n, consumer);
+    }
+
+    /**
+     * Call an IntConsumer with a range of Integers, starting at lower and ending exclusively at upper.
+     *
+     * @param lower    lower bound inclusive
+     * @param upper    upper bound, exclusive.
+     * @param consumer consumer the IntConsumer invoked with each value in the range
+     */
+    public static void invokeRange(int lower, int upper, IntConsumer consumer) {
+        //TODO Add error handling
+        IntStream.range(lower, upper).forEach(consumer);
+    }
+
+    /**
+     * invoke a Supplier n times, discarding the results
+     *
+     * @param n        the number of invocations
+     * @param supplier the Supplier to obtain values to discard
+     */
+    public static void tossN(int n, Supplier<?> supplier) {
+        //TODO Add error handling
+        streamOf(supplier).limit(Math.max(n, 0)).forEach(Supplier::get);
+    }
+
+    /**
+     * Invoke a Supplier n times, returning the result of the invocation in a List.
+     *
+     * @param n        the times to invoke the Supplier and the length of the returned List
+     * @param supplier the Supplier from which to obtain values
+     * @param <T>      the type of the things retrieved from the Supplier and returned in the List
+     * @return a list of resulting in order of invocation results
+     */
+    public static <T> List<T> getN(int n, Supplier<T> supplier) {
+        //TODO Add error handling
+        return limitedSupplier(n, supplier).collect(Collectors.toList());
+    }
+
+    /**
+     * Invoke a Supplier n times invoking the supplied Consumer with the result.
+     *
+     * @param n        the times to invoke the supplier and subsequently the Consumer
+     * @param supplier the Supplier from which to obtain values
+     * @param <T>      the type of the things retrieved from the Supplier submitted to the Consumer
+     */
+    public static <T> void pipeN(int n, Supplier<T> supplier, Consumer<T> consumer) {
+        //TODO Add error handling
+        limitedSupplier(n, supplier).forEach(consumer);
+    }
+
+    /**
+     * Invoke a Supplier n times, returning the results as a Stream. An internal utility method.
+     * If an invocation is
+     *
+     * @param n        the times to invoke the Supplier
+     * @param supplier the Supplier from which to obtain values
+     * @param <T>      the type of the things retrieved from the Supplier and returned in the Stream
+     * @return a Stream of values in order of invocation results
+     */
+    private static <T> Stream<T> limitedSupplier(int n, Supplier<T> supplier) {
+        return streamOf(supplier).limit(Math.max(n, 0)).map(i -> supplier.get());
+    }
+}
---
+++ src/test/java/net/littlethunder/util/repeat/RepeatTest.java
@@ -0,0 +1,125 @@
+package net.littlethunder.util.repeat;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.Consumer;
+import java.util.stream.Stream;
+
+import static java.util.Collections.EMPTY_LIST;
+import static net.littlethunder.util.repeat.Repeat.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+class RepeatTest {
+
+    private static final String FOO = "foo";
+
+    /**
+     * Test Repeat listOf function
+     */
+    @Test
+    void listOfTest() {
+        List<String> list = listOf(FOO, 5);
+        assertEquals(5, list.size()); //simple case
+        for (String s : list) {
+            assertSame(s, FOO); //check instance is the same
+        }
+        list = listOf(FOO, 0);  //check 0 n parameter
+        assertTrue(list.isEmpty());
+        list = listOf(FOO, -1); ///check negative parameter
+        assertTrue(list.isEmpty());
+        list = listOf(null, 4); //check making list of nulls
+        assertEquals(4, list.size());
+        for (String s : list) {
+            assertNull(s);
+        }
+    }
+
+    @SuppressWarnings("StringEquality")
+    @Test
+    void streamOfTest() {
+        Stream<String> s = streamOf(FOO);
+        assertNotNull(s);
+        assertSame(FOO, streamOf(FOO).findFirst().orElse(null));
+        assertEquals(0, streamOf(FOO).limit(200).filter(f -> FOO != f).count());
+        assertEquals(0, streamOf(null).limit(200).filter(f -> f != null).count());
+    }
+
+    @Test
+    void invokeRangeUpperLowerTest() {
+        List<Integer> list = new ArrayList<>();
+        invokeRange(0, 5, list::add);
+        assertEquals(List.of(0, 1, 2, 3, 4), list);
+        list.clear();
+        invokeRange(0, 0, list::add);
+        assertEquals(EMPTY_LIST, list);
+        list.clear();
+        invokeRange(2, 5, list::add);
+        assertEquals(List.of(2, 3, 4), list);
+        list.clear();
+        invokeRange(-2, 5, list::add);
+        assertEquals(List.of(-2, -1, 0, 1, 2, 3, 4), list);
+        list.clear();
+        invokeRange(5, 0, list::add);
+        assertEquals(EMPTY_LIST, list);
+    }
+
+    @Test
+    void testInvokeRangeTest() {
+        List<Integer> list = new ArrayList<>();
+        invokeRange(5, list::add);
+        assertEquals(List.of(0, 1, 2, 3, 4), list);
+        list.clear();
+        invokeRange(0, list::add);
+        assertEquals(EMPTY_LIST, list);
+        list.clear();
+        invokeRange(5, list::add);
+        assertEquals(List.of(0, 1, 2, 3, 4), list);
+        list.clear();
+        invokeRange(-5, list::add);
+        assertEquals(EMPTY_LIST, list);
+        list.clear();
+    }
+
+    @Test
+    void tossNTest() {
+        AtomicInteger i = new AtomicInteger();
+        tossN(5, i::incrementAndGet);
+        assertEquals(5, i.get());
+        tossN(0, i::incrementAndGet);
+        assertEquals(5, i.get());
+        tossN(-1, i::incrementAndGet);
+        assertEquals(5, i.get());
+    }
+
+    @Test
+    void getNTest() {
+        AtomicInteger i = new AtomicInteger();
+        assertEquals(List.of(1, 2, 3, 4, 5), getN(5, i::incrementAndGet));
+        assertEquals(List.of(6), getN(1, i::incrementAndGet));
+        assertEquals(EMPTY_LIST, getN(0, i::incrementAndGet));
+        assertEquals(EMPTY_LIST, getN(-1, i::incrementAndGet));
+    }
+
+    @Test
+    void pipeNTest() {
+        AtomicInteger i = new AtomicInteger();
+        List<Integer> list = new ArrayList<>();
+        Consumer<Integer> c = new Consumer<Integer>() {
+            @Override
+            public void accept(Integer integer) {
+                list.add(integer);
+            }
+        };
+        pipeN(5, i::incrementAndGet, c);
+        assertEquals(List.of(1, 2, 3, 4, 5), list);
+        pipeN(1, i::incrementAndGet, c);
+        assertEquals(List.of(1,2,3,4,5,6), list);
+        pipeN(0, i::incrementAndGet, c);
+        assertEquals(List.of(1,2,3,4,5,6), list);
+        pipeN(-1, i::incrementAndGet, c);
+        assertEquals(List.of(1,2,3,4,5,6), list);
+    }
+}

4c06c70a97340dfb18b94fd44626eb0eb99b1950
wyldbill <wyldbill.220@gmail.com>
2021-09-30 09:36:23 -0500

    Javadoc, module and package
    Update JavaDoc typo in Repeat.java
    Add package-info.java
    Create util_repeat module

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module util_repeat {                //not open, no reflection permitted
+    //requires <none>;              //no runtime dependencies
+    //requires static <none>;       //no compiletime dependencies
+    //requires transitive <none>;   //no transitive dependencies
+    //uses <none>;                  //no external services consumed
+    //provides <none>;              //we export no abstract classes or interfaces, nor implementations via provides...with
+    //opens <none>;                 //no reflective access, even conditionally via opens ... to
+    exports net.ltltd.util.repeat;  //eveything we are - no restriction via exports ... to
+
+}
---
+++ src/main/java/net/ltltd/util/repeat/package-info.java
@@ -0,0 +1,13 @@
+/**
+ *<p>net.ltltd.util.repeat is a collection of utility methods which repeat things. It creates repetitive collections,
+ * invokes things multiple times, streams things repetitively, etc.
+ *</p>
+ * <p>In order to remain compact, and facilitate minimizing jar and runtime size, it imports a minimum of java classes
+ * and is entirely free of external dependencies.
+ *</p>
+ *
+ *  * @since 0.0.2
+ *  * @author wyldbill
+ *  * @version 0.0.2
+ */
+package net.ltltd.util.repeat;
